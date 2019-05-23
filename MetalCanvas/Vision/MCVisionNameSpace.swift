04fcf36375db6b8a34b8fd5cf71a817b7529a08b
hideyuki machida <h.machida@cchan.tv>
2019-05-23 23:05:08 +0900

    Add Vision

---
+++ MetalCanvas/Vision/MCVisionNameSpace.swift
@@ -0,0 +1,18 @@
+//
+//  MCVisionNameSpace.swift
+//  MetalCanvas
+//
+//  Created by hideyuki machida on 2019/01/14.
+//  Copyright © 2019 hideyuki machida. All rights reserved.
+//
+
+import Foundation
+
+public struct MCVision {
+	public enum ErrorType: Error {
+		case rendering
+	}
+
+	public struct Depth { }
+	public struct Detection { }
+}
