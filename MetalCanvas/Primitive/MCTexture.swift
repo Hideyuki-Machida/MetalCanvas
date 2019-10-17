0c2475faadf95e3fd001f22482e290eecaad9258
hideyuki machida <hideyuki.machida.jp@gmail.com>
2019-10-17 09:50:46 +0900

    update

--- MetalCanvas/Primitive/MCTexture.swift
+++ MetalCanvas/Primitive/MCTexture.swift
@@ -41,6 +41,15 @@ public struct MCTexture {
         self.texture = try MCCore.textureLoader.newTexture(cgImage: cgImage, options: textureLoaderOptions)
     }
 
+    public init(image: CGImage, SRGB: Bool = true) throws {
+        let textureLoaderOptions: [MTKTextureLoader.Option: Any] = [
+            MTKTextureLoader.Option.SRGB: SRGB,
+            MTKTextureLoader.Option.textureUsage: NSNumber(value: MTLTextureUsage.shaderRead.rawValue),
+            MTKTextureLoader.Option.textureStorageMode: NSNumber(value: MTLStorageMode.private.rawValue)
+        ]
+        self.texture = try MCCore.textureLoader.newTexture(cgImage: image, options: textureLoaderOptions)
+    }
+    
     public init(URL: URL, SRGB: Bool = true) throws {
 		let textureLoaderOptions: [MTKTextureLoader.Option: Any] = [
 			MTKTextureLoader.Option.SRGB: SRGB,
