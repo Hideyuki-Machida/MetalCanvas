164e8d78425877209b2f68242ca5a60928663e11
hideyuki machida <hideyuki.machida.jp@gmail.com>
2019-10-19 02:33:33 +0900

    update render

--- MetalCanvas/MCImageRenderView.swift
+++ MetalCanvas/MCImageRenderView.swift
@@ -114,90 +114,66 @@ extension MCImageRenderView {
 		var commandBuffer: MTLCommandBuffer = commandBuffer
 		////////////////////////////////////////////////////////////
 
-		////////////////////////////////////////////////////////////
-		//
-		do {
-			self.drawableSize = renderSize
+        ////////////////////////////////////////////////////////////
+        // drawableSizeを最適化
+        self.drawableSize = renderSize
+        ////////////////////////////////////////////////////////////
+        
+        if self.hasHEVCHardwareEncoder {
+            // MPSが使える端末
+            
+            ////////////////////////////////////////////////////////////
+            // previewScale encode
+            let scale: Double = Double(drawable.texture.width) / Double(texture.width)
+            var transform: MPSScaleTransform = MPSScaleTransform(scaleX: scale, scaleY: scale, translateX: 0, translateY: 0)
+            withUnsafePointer(to: &transform) { [weak self] (transformPtr: UnsafePointer<MPSScaleTransform>) -> () in
+                self?.filter.scaleTransform = transformPtr
+                self?.filter.encode(commandBuffer: commandBuffer, sourceTexture: texture, destinationTexture: drawable.texture)
+            }
+            ////////////////////////////////////////////////////////////
 
-			if drawable.texture.width != texture.width || drawable.texture.height != texture.height {
-				if drawable.texture.width < texture.width || drawable.texture.height < texture.height {
-					guard let cb: MTLCommandBuffer = MCCore.commandQueue.makeCommandBuffer() else { return }
-					let t: MCTexture = try MCTexture.init(renderSize: CGSize.init(width: drawable.texture.width, height: drawable.texture.height))
-					let blitEncoder: MTLBlitCommandEncoder? = cb.makeBlitCommandEncoder()
-					blitEncoder?.copy(from: t.texture,
-									 sourceSlice: 0,
-									 sourceLevel: 0,
-									 sourceOrigin: MTLOrigin(x: 0, y: 0, z: 0),
-									 sourceSize: MTLSizeMake(drawable.texture.width, drawable.texture.height, t.texture.depth),
-									 to: drawable.texture,
-									 destinationSlice: 0,
-									 destinationLevel: 0,
-									 destinationOrigin: MTLOrigin(x: 0, y: 0, z: 0))
-					blitEncoder?.endEncoding()
-					cb.addCompletedHandler { [weak self] (cb) in
-						self?.draw()
+            ////////////////////////////////////////////////////////////
+            // commit
+            commandBuffer.present(drawable)
+            commandBuffer.commit()
+            self.draw()
+            ////////////////////////////////////////////////////////////
+        } else {
+            // MPSが使えない端末
 
-                        guard let drawable: CAMetalDrawable = self?.currentDrawable else {
-							commandBuffer.commit()
-							return
-						}
-						self?.updatePixelBuffer002(commandBuffer: &commandBuffer, drawable: drawable, texture: texture, renderSize: renderSize)
-					}
-					cb.present(drawable)
-					cb.commit()
-                    cb.waitUntilCompleted()
-				} else {
-					guard let cb: MTLCommandBuffer = MCCore.commandQueue.makeCommandBuffer() else { return }
-					let t: MCTexture = try MCTexture.init(renderSize: CGSize.init(width: renderSize.width, height: renderSize.height))
-					let blitEncoder: MTLBlitCommandEncoder? = cb.makeBlitCommandEncoder()
-					blitEncoder?.copy(from: t.texture,
-									 sourceSlice: 0,
-									 sourceLevel: 0,
-									 sourceOrigin: MTLOrigin(x: 0, y: 0, z: 0),
-									 sourceSize: MTLSizeMake(t.width, t.height, t.texture.depth),
-									 to: drawable.texture,
-									 destinationSlice: 0,
-									 destinationLevel: 0,
-									 destinationOrigin: MTLOrigin(x: 0, y: 0, z: 0))
-					blitEncoder?.endEncoding()
-					cb.addCompletedHandler { [weak self] (cb) in
-                        self?.draw()
+            do {
+                ////////////////////////////////////////////////////////////
+                // previewScale encode
+                let texture: MCTexture = try MCTexture.init(texture: texture)
+                var mcTexture01: MCTexture = try MCTexture.init(texture: drawable.texture)
+                let scale: Float = Float(mcTexture01.width) / Float(texture.width)
+                let canvas: MCCanvas = try MCCanvas.init(destination: &mcTexture01, orthoType: MCCanvas.OrthoType.topLeft)
+                let imageMat: MCGeom.Matrix4x4 = MCGeom.Matrix4x4.init(scaleX: scale, scaleY: scale, scaleZ: 1.0)
+                
+                try canvas.draw(commandBuffer: &commandBuffer, objects: [
+                    try MCPrimitive.Image.init(
+                        texture: texture,
+                        ppsition: MCGeom.Vec3D.init(x: Float(mcTexture01.width) / 2.0, y: Float(mcTexture01.height) / 2.0, z: 0),
+                        transform: imageMat,
+                        anchorPoint: .center
+                    )
+                ])
+                ////////////////////////////////////////////////////////////
+                
+                ////////////////////////////////////////////////////////////
+                // commit
+                commandBuffer.present(drawable)
+                commandBuffer.commit()
+                self.draw()
+                ////////////////////////////////////////////////////////////
 
-						guard let drawable: CAMetalDrawable = self?.currentDrawable else {
-							commandBuffer.commit()
-							return
-						}
-                        self?.updatePixelBuffer002(commandBuffer: &commandBuffer, drawable: drawable, texture: texture, renderSize: renderSize)
-					}
-                    cb.present(drawable)
-					cb.commit()
-                    cb.waitUntilCompleted()
-				}
-			} else {
-				self.updatePixelBuffer002(commandBuffer: &commandBuffer, drawable: drawable, texture: texture, renderSize: renderSize)
-			}
-		} catch {
-			commandBuffer.commit()
-		}
+            } catch {
+                commandBuffer.commit()
+            }
+        }
+        
 	}
-	
-	fileprivate func updatePixelBuffer002(commandBuffer: inout MTLCommandBuffer, drawable: CAMetalDrawable, texture: MTLTexture, renderSize: CGSize) {
-		let blitEncoder: MTLBlitCommandEncoder? = commandBuffer.makeBlitCommandEncoder()
-		blitEncoder?.copy(from: texture,
-						 sourceSlice: 0,
-						 sourceLevel: 0,
-						 sourceOrigin: MTLOrigin(x: 0, y: 0, z: 0),
-						 sourceSize: MTLSizeMake(texture.width, texture.height, texture.depth),
-						 to: drawable.texture,
-						 destinationSlice: 0,
-						 destinationLevel: 0,
-						 destinationOrigin: MTLOrigin(x: 0, y: 0, z: 0))
-		blitEncoder?.endEncoding()
 
-		commandBuffer.present(drawable)
-		commandBuffer.commit()
-		self.draw()
-	}
 }
 
 
